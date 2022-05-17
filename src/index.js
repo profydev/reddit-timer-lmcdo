8e076e191e55e5686892bf8ad76cf829a40f1078
Lawrence McDonell <lawrence.mcdonell@gmail.com>
2022-05-17 14:36:54 +1000

    Adds fonts, resets browser styles, adds global styles, router with / and /search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import App from './App';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="search"> </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
